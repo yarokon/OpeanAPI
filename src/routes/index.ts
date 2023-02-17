6f3de188f354e43dafab1bd62632bac5f11aedd3
Yaroslav Shuhailo <yaroslav@redocly.com>
2023-02-17 14:38:49 +0200

    chore: added slack router

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,5 +2,6 @@ import combineRouters from 'koa-combine-routers';
 
 import { userRouter } from './user.js';
 import { healthRouter } from './health.js';
+import { slackRouter } from './slack.js';
 
-export const router = combineRouters(userRouter, healthRouter);
+export const router = combineRouters(userRouter, healthRouter, slackRouter);
---
+++ src/routes/slack.ts
@@ -0,0 +1,19 @@
+import Router from 'koa-router';
+import axios from 'axios';
+
+const slackRouter = new Router({ prefix: '/svix' });
+
+slackRouter.post('/slack', async (ctx) => {
+  const SLACK_PROD_URL =
+    'https://hooks.slack.com/services/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';
+
+  const slackAdapter = (payload: unknown) => ({
+    text: JSON.stringify(payload, null, 2),
+  });
+
+  const response = await axios.post(SLACK_PROD_URL, slackAdapter(ctx.request.body));
+
+  ctx.body = response.data;
+});
+
+export { slackRouter };
